0e3de53b13a27dfecde459c2a9a01ae9c7747876
baoobab <v.xxl.90@mail.ru>
2025-03-14 15:41:27 +0300

    add, rm colshape

--- src/server/commands.ts
+++ src/server/commands.ts
@@ -3,12 +3,12 @@
 
 mp.events.addCommand("spawncar", (player, carName = "") => {  
   if (!Object.keys(VEHICLE_NAMES).includes(carName)) {
-    return player.notify('No such vehicle');
+    return player.outputChatBox('No such vehicle');
   }
 
 	const vehicle = mp.vehicles.new(mp.joaat(carName), player.position); //Creating the vehicle
 
-	//Adding a custom method to the vehicle which will handle the stream in (will be called from the client).
+	// Adding a custom method to the vehicle which will handle the stream in (will be called from the client).
 	vehicle.onStreamIn = (veh: VehicleMp) => { //supports async as well
     
 	  if (!player || !mp.players.exists(player)) return; //if the player is no longer available when this method is called we return here.
@@ -20,7 +20,46 @@ mp.events.addCommand("spawncar", (player, carName = "") => {
 
 })
 
-//Adding a event which will be called from client when the vehicle streams in
+mp.events.addCommand("addcolshape", (player, dimensions: string) => {
+  if (!dimensions || dimensions.split(" ").length < 3) {
+    return player.outputChatBox(`Bad dimensions ${dimensions}`)
+  }
+  const [width, height, depth] = dimensions.split(" ").map((_) => {return Number(_)})
+
+  mp.colshapes.newCuboid(
+    player.position.x, // X-Координаты центра
+    player.position.y, // Y-Координаты центра
+    player.position.z, // Z-Координаты центра
+    depth, // Длина
+    width, // Ширина
+    height // Высота
+  );
+
+})
+
+mp.events.addCommand("rmcolshape", (player, fullText) => {
+  if (!fullText || isNaN(Number(fullText))) {
+    return player.outputChatBox(`Bad input`)
+  }
+  const shapeId = Number(fullText)
+  let isDestroyed = false
+
+  mp.colshapes.forEach((shape) => {
+    if (shape.id === shapeId) {
+      shape.destroy()
+      isDestroyed = true
+      return
+    }
+  })
+  
+  if (isDestroyed) {
+    return player.outputChatBox(`Colshape ${shapeId} destroyed`)
+  }
+
+  return player.outputChatBox(`Colshape not found`)
+})
+
+// Adding a event which will be called from client when the vehicle streams in
 mp.events.add('server::vehicleStreamIn', async (_player, remoteid) => {
 	const vehicle = mp.vehicles.at(remoteid);
 
