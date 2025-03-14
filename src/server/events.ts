be75e84a793c5709dc11db6ab9c0ee1afe94e773
baoobab <v.xxl.90@mail.ru>
2025-03-14 18:31:03 +0300

    CarMarket events: enter, exit

--- src/server/events.ts
+++ src/server/events.ts
@@ -1,4 +1,5 @@
 ﻿import { SPAWNPOINTS } from '@shared/constants';
+import CarMarket, { carMarkets } from './carMarket';
 
 mp.events.add('playerDeath', (player) => {
 	const randomSpawnPoint = new mp.Vector3(
@@ -11,8 +12,28 @@ mp.events.add('playerDeath', (player) => {
 
 mp.events.add('playerEnterColshape', (player: PlayerMp, colshape: ColshapeMp) => {	
 	player.outputChatBox(`You entered the CUBOID zone`);
+
+  // TODO: need thinking - mb colshape.getVariable("isCarMarket") === true (bad: abstraction will depends on details???)		
+	mp.events.call("playerEnterCarMarket", player, carMarkets.filter((market) => market.colshape.id === colshape.id)[0])
 });
 
 mp.events.add('playerExitColshape', (player, colshape) => {
+	// TODO: need thinking - mb colshape.getVariable("isCarMarket") === true (bad: abstraction will depends on details???)		
+	mp.events.call("playerExitCarMarket", player, carMarkets.filter((market) => market.colshape.id === colshape.id)[0])
+
 	player.outputChatBox(`You leaved the CUBOID zone`);
+});
+
+mp.events.add('playerEnterCarMarket', (player: PlayerMp, carMarket: CarMarket) => {	
+	if (!carMarket) return;	
+
+	player.outputChatBox(`You entered the CARMARKET zone`);
+  carMarket.enter(player)
+});
+
+mp.events.add('playerExitCarMarket', (player: PlayerMp, carMarket: CarMarket) => {
+	if (!carMarket) return;
+
+  carMarket.leave(player)
+	player.outputChatBox(`You leaved the CARMARKET zone`);
 });
