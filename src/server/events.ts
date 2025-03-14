e2cf1e2ebc7c1d3bcd179825de177e5331c296fa
baoobab <v.xxl.90@mail.ru>
2025-03-14 15:41:56 +0300

    leave, enter colshape events

--- src/server/events.ts
+++ src/server/events.ts
@@ -3,6 +3,20 @@
 mp.events.add('playerDeath', (player) => {
 	const randomSpawnPoint = new mp.Vector3(
 		SPAWNPOINTS.SpawnPoints[Math.floor(Math.random() * SPAWNPOINTS.SpawnPoints.length)])
+
+		player.outputChatBox(`spawnpoint: ${randomSpawnPoint}`)
 		player.spawn(randomSpawnPoint);
 		player.health = 100;
+});
+
+mp.events.add('playerEnterColshape', (player, colshape) => {
+  if (colshape.shapeType === RageEnums.ColshapeType.CUBOID) {
+    player.outputChatBox(`You entered the CUBOID zone`);
+  }
+});
+
+mp.events.add('playerExitColshape', (player, colshape) => {
+  if (colshape.shapeType === RageEnums.ColshapeType.CUBOID) {
+    player.outputChatBox(`You leaved the CUBOID zone`);
+  }
 });
