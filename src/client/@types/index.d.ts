de50fdcb2be11d8d5a43de1bfde2d91dd8eddc58
baoobab <v.xxl.90@mail.ru>
2025-03-16 18:32:26 +0300

    custom types from server

--- src/client/@types/index.d.ts
+++ src/client/@types/index.d.ts
@@ -1,6 +1,18 @@
+import { CustomEntityType } from "@shared/constants";
+
 declare global {
-  interface PlayerMp {
+	interface ColshapeMp {
+		isForCustomEntityType: boolean; // Is the colshape refers to CustomEntityType
+		customEntityType: CustomEntityType | undefined; // What custom type owns this colshape.
+	}
+
+	interface PlayerMp {
+		money: number; // balance on the bank account
 		ownVehicles: VehicleMp[]; // vehicles in ownership
+	}
+	
+	interface VehicleMp {
+		onStreamIn(vehicle: VehicleMp): void;
 	}
 }
 
