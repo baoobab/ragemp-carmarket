71435b3b88486b1b4a0ea43fd87c06dd82addb5b
baoobab <v.xxl.90@mail.ru>
2025-03-16 23:27:57 +0300

    refactor: commons code style

--- src/server/common/events.ts
+++ src/server/common/events.ts
@@ -1,8 +1,7 @@
 ﻿import { CustomEntityType, SPAWNPOINTS } from '@shared/constants';
 import CarMarket from '../modules/car-market';
-import { carMarketsPool } from '../pools/car-market.pool'
+import { carMarketsPool } from '@/pools/car-market.pool';
 import SellPoint from '../modules/sell-point';
-
 
 mp.events.add('playerReady', (player) => {
 	if (!player || !mp.players.exists(player)) return;
@@ -16,79 +15,77 @@ mp.events.add('playerReady', (player) => {
 	// Custom method to spawn a car in player position (just keep in mind - each player is admin for now)
 	player.spawnCar = (carName: RageEnums.Hashes.Vehicle) => {
 		if (player.vehicle) {
-			player.outputChatBox("You need to leave the current vehicle")
+			player.outputChatBox('You need to leave the current vehicle');
 			return null;
 		}
 
 		const vehicle = mp.vehicles.new(carName, player.position); // Creating the vehicle
-	
+
 		// Adding a custom method to the vehicle which will handle the stream in (will be called from the client).
-		vehicle.onStreamIn = (veh: VehicleMp) => { // Supports async as well
-			
+		vehicle.onStreamIn = (veh: VehicleMp) => {
+			// Supports async as well
+
 			if (!player || !mp.players.exists(player)) return; // If the player is no longer available when this method is called we return here.
-	
-			setTimeout(() => {player.putIntoVehicle(veh, 0)}, 200) // Put the player into the vehicle as soon as the vehicle is streamed in.
-		}
+
+			setTimeout(() => {
+				player.putIntoVehicle(veh, 0);
+			}, 200); // Put the player into the vehicle as soon as the vehicle is streamed in.
+		};
 		player.position = vehicle.position; // Setting player position to the vehicle position
 
 		return vehicle;
-	}
+	};
 
 	// Custom method to check - is player a driver now
 	player.isDriver = (): boolean => {
 		return !!player.vehicle && player.seat === RageEnums.VehicleSeat.DRIVER;
-	}
+	};
 
 	// Custom methos to tp player to the target vehicle (just keep in mind - each player is admin for now)
 	player.teleportToDriverDoor = (vehicle: VehicleMp): void => {
-		const offset = vehicle.isRightHandDrive() 
+		const offset = vehicle.isRightHandDrive()
 			? new mp.Vector3(-1.2, 0, 0) // right offset
 			: new mp.Vector3(1.2, 0, 0); // left offset
-	
-		const position = vehicle.position.add(
-			offset
-		);
-	
-		player.position = position;
-	}
+
+		player.position = vehicle.position.add(offset);
+	};
 });
 
 mp.events.add('playerDeath', (player) => {
-	const randomSpawnPoint = new mp.Vector3(
-		SPAWNPOINTS.SpawnPoints[Math.floor(Math.random() * SPAWNPOINTS.SpawnPoints.length)])
+	const randomSpawnPoint = new mp.Vector3(SPAWNPOINTS.SpawnPoints[Math.floor(Math.random() * SPAWNPOINTS.SpawnPoints.length)]);
 
-		player.outputChatBox(`spawnpoint: ${randomSpawnPoint}`)
-		player.spawn(randomSpawnPoint);
-		player.health = 100;
+	player.outputChatBox(`spawnpoint: ${randomSpawnPoint}`);
+	player.spawn(randomSpawnPoint);
+	player.health = 100;
 });
 
-mp.events.add('playerEnterColshape', (player: PlayerMp, colshape: ColshapeMp) => {	
+mp.events.add('playerEnterColshape', (player: PlayerMp, colshape: ColshapeMp) => {
 	if (!colshape.isForCustomEntityType) {
 		return player.outputChatBox(`You entered the colshape#${colshape.id}`);
 	}
 
-	const args: any[] = []
+	const args: any[] = [];
 	switch (colshape.customEntityType) {
 		case CustomEntityType.CAR_MARKET: {
-			const targetMarket = carMarketsPool.filter((market) => market.colshape.id === colshape.id)[0] // What if >1 markets on position?
-			player.setVariable("currentCarMarketColshapeId", targetMarket.colshape.id)
+			const targetMarket = carMarketsPool.filter((market) => market.colshape.id === colshape.id)[0]; // What if >1 markets on position?
+			player.setVariable('currentCarMarketColshapeId', targetMarket.colshape.id);
 
-			args.push(targetMarket)
+			args.push(targetMarket);
 			break;
 		}
-		case CustomEntityType.SELL_POINT: {			
-			const marketColshapeId = player.getVariable<number>("currentCarMarketColshapeId") // base case: SellPoints only used in CarMarkets			
+		case CustomEntityType.SELL_POINT: {
+			const marketColshapeId = player.getVariable<number>('currentCarMarketColshapeId'); // base case: SellPoints only used in CarMarkets
 			if (marketColshapeId === null) break;
-			
-			const currentCarMarket = carMarketsPool.filter((market) => market.colshape.id === marketColshapeId)[0]			
-			const targetSellPoint = currentCarMarket.sellPointByColshapeId(colshape.id)
 
-			player.setVariable("currentSellPointColshapeId", targetSellPoint?.colshape.id)
-			args.push(targetSellPoint)
+			const currentCarMarket = carMarketsPool.filter((market) => market.colshape.id === marketColshapeId)[0];
+			const targetSellPoint = currentCarMarket.sellPointByColshapeId(colshape.id);
+
+			player.setVariable('currentSellPointColshapeId', targetSellPoint?.colshape.id);
+			args.push(targetSellPoint);
 			break;
 		}
 	}
-	mp.events.call(`playerEnter${colshape.customEntityType}`, player, ...args)
+	mp.events.call(`playerEnter${colshape.customEntityType}`, player, ...args);
 });
 
 mp.events.add('playerExitColshape', (player, colshape) => {
@@ -96,59 +93,58 @@ mp.events.add('playerExitColshape', (player, colshape) => {
 		return player.outputChatBox(`You leaved the colshape#${colshape.id}`);
 	}
 
-	const args: any[] = []
+	const args: any[] = [];
 	switch (colshape.customEntityType) {
 		case CustomEntityType.CAR_MARKET: {
-			const marketId = player.getVariable<number>("currentCarMarketColshapeId")
+			const marketId = player.getVariable<number>('currentCarMarketColshapeId');
 			if (marketId === null) break;
 
-			const targetMarket = carMarketsPool.filter((market) => market.colshape.id === marketId)[0]
+			const targetMarket = carMarketsPool.filter((market) => market.colshape.id === marketId)[0];
 
-			player.setVariable("currentCarMarketColshapeId", null)
-			args.push(targetMarket)
+			player.setVariable('currentCarMarketColshapeId', null);
+			args.push(targetMarket);
 			break;
 		}
 		case CustomEntityType.SELL_POINT: {
-			const sellPointColshapeId = player.getVariable<number>("currentSellPointColshapeId")
+			const sellPointColshapeId = player.getVariable<number>('currentSellPointColshapeId');
 			if (sellPointColshapeId === null) break;
 
-			const targetCarMarket = carMarketsPool.filter((market) => !!market.sellPointByColshapeId(sellPointColshapeId))[0]
-			const targetSellPoint = targetCarMarket.sellPointByColshapeId(sellPointColshapeId)
+			const targetCarMarket = carMarketsPool.filter((market) => !!market.sellPointByColshapeId(sellPointColshapeId))[0];
+			const targetSellPoint = targetCarMarket.sellPointByColshapeId(sellPointColshapeId);
 
-			player.setVariable("currentSellPointColshapeId", null)
-			args.push(targetSellPoint)
+			player.setVariable('currentSellPointColshapeId', null);
+			args.push(targetSellPoint);
 			break;
 		}
 	}
-	mp.events.call(`playerExit${colshape.customEntityType}`, player, ...args)
+	mp.events.call(`playerExit${colshape.customEntityType}`, player, ...args);
 });
 
-mp.events.add('playerEnterSellPoint', (player: PlayerMp, sellPoint: SellPoint<VehicleMp>) => {	
-	if (sellPoint === null) return;	
+mp.events.add('playerEnterSellPoint', (player: PlayerMp, sellPoint: SellPoint<VehicleMp>) => {
+	if (sellPoint === null) return;
 
-	player.outputChatBox(`You entered the SELLPOINT ${sellPoint.marker?.label || ""}`);
-	sellPoint.enter(player)
+	player.outputChatBox(`You entered the SELLPOINT ${sellPoint.marker?.label || ''}`);
+	sellPoint.enter(player);
 });
 
-mp.events.add('playerExitSellPoint', (player: PlayerMp, sellPoint: SellPoint<VehicleMp>) => {	
-	if (sellPoint === null) return;	
+mp.events.add('playerExitSellPoint', (player: PlayerMp, sellPoint: SellPoint<VehicleMp>) => {
+	if (sellPoint === null) return;
 
-	player.outputChatBox(`You leaved the SELLPOINT ${sellPoint.marker?.label || ""}`);
-	sellPoint.leave(player)
+	player.outputChatBox(`You leaved the SELLPOINT ${sellPoint.marker?.label || ''}`);
+	sellPoint.leave(player);
 });
 
-
-mp.events.add('playerEnterCarMarket', (player: PlayerMp, carMarket: CarMarket) => {	
-	if (carMarket === null) return;	
+mp.events.add('playerEnterCarMarket', (player: PlayerMp, carMarket: CarMarket) => {
+	if (carMarket === null) return;
 
 	player.outputChatBox(`You entered the CARMARKET#${carMarket.colshape.id}: ${carMarket.title}`);
-  carMarket.enter(player)
+	carMarket.enter(player);
 });
 
 mp.events.add('playerExitCarMarket', (player: PlayerMp, carMarket: CarMarket) => {
 	if (!carMarket) return;
 
-  carMarket.exit(player)
+	carMarket.exit(player);
 	player.outputChatBox(`You leaved the CARMARKET#${carMarket.colshape.id}: ${carMarket.title}`);
 });
 
@@ -162,12 +158,12 @@ mp.events.add('server::vehicleStreamIn', async (_player, remoteid) => {
 	// Custom method to check vehicle steering-hand type
 	vehicle.isRightHandDrive = (): boolean => {
 		const model = vehicle.model;
-		
+
 		const RHD_MODELS = new Set([
-			mp.joaat(''),
+			mp.joaat('')
 			// or any right-hand car
 		]);
-	
+
 		return RHD_MODELS.has(model);
 	};
 
